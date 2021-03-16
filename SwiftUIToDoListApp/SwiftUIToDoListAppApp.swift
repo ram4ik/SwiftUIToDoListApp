4133cd936bf7d08e0b8e909497d53dd6b683f661
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-16 13:32:39 +0200

    Initial Commit

---
+++ SwiftUIToDoListApp/SwiftUIToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListAppApp.swift
+//  SwiftUIToDoListApp
+//
+//  Created by ramil on 16.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
