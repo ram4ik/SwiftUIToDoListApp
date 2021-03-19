f44071ad4a11edb774bb66b4a1902e6a3a02fa34
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-19 15:28:00 +0200

    View Model

--- SwiftUIToDoListApp/Views/AddView.swift
+++ SwiftUIToDoListApp/Views/AddView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -18,9 +23,7 @@ struct AddView: View {
                     .frame(height: 55)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(10)
-                Button(action: {
-                    
-                }, label: {
+                Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -32,6 +35,27 @@ struct AddView: View {
             }.padding(14)
         }
         .navigationTitle("Add an Item ✏️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long! 😋"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -40,5 +64,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
